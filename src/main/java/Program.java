f64835cd49f35c59fd24a373acd0407074868f76
vvinod <vvinod@tavisca.in>
2019-08-08 18:40:17 +0530

    Initial commit

---
+++ src/main/java/Program.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Program {
+    public static void main(String[] args) {
+        System.out.println(SequenceFinder.findSequence( new int[] {0, 1, 2, 4, 3, 5})==27? "PASS":"FAIL");
+        System.out.println(SequenceFinder.findSequence( new int[] {-1, 1, 2, 3})==6? "PASS":"FAIL");
+        System.out.println(SequenceFinder.findSequence( new int[] {-1})==-1? "PASS":"FAIL");
+        System.out.println(SequenceFinder.findSequence( new int[] {-1, 0, 1})==1? "PASS":"FAIL");
+        System.out.println(SequenceFinder.findSequence( new int[] {1,1})==2? "PASS":"FAIL");
+
+    }
+}
---
+++ src/main/java/SequenceFinder.java
@@ -0,0 +1,7 @@
+public class SequenceFinder {
+
+    public static int findSequence(int[] ints) {
+        int sum=0;
+        return sum;
+    }
+}
