83533cba342e2e3b4c0547c12c30867a4a1ebcf8
vvinod <vvinod@tavisca.in>
2019-08-10 11:08:09 +0530

    dealing with negative numbers done

--- src/main/java/SequenceFinder.java
+++ src/main/java/SequenceFinder.java
@@ -1,11 +1,54 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class SequenceFinder {
     public static int findSequence(int[] ints) {
         int sum=0;
         int numberOfNegatives=findNegatives(ints);
+        Arrays.sort(ints);
+        if(numberOfNegatives%2==0)
+        {
+            for(int i = 0 ; i<numberOfNegatives;i++)
+            {
+                sum=sum+(ints[i]*ints[i+1]);
+                i++;
+            }
+        }
+        else
+        {
+            if(Arrays.binarySearch(ints,0)>=0)
+            {
+                for(int i=0; i<numberOfNegatives-1;i++)
+                {
+                    System.out.println("inside");
+                    sum=sum+(ints[i]*ints[i+1]);
+                    i++;
+                }
+            }
+            else
+            {
+                for(int i=0; i<numberOfNegatives-1;i++)
+                {
+                    System.out.println("inside");
+                    sum=sum+(ints[i]*ints[i+1]);
+                    i++;
+                }
+                sum=sum+ints[numberOfNegatives-1];
+            }
+        }
+        System.out.print(sum);
         return sum;
     }
-    public int findNegatives(int[] ints)
+    public static int findNegatives(int[] ints)
     {
-
+        int count=0;
+        for(int i:ints)
+        {
+            if(i<0)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
