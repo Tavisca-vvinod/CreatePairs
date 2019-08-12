6d301656ac49fe73a4d12a854e7b93ed21414e84
vvinod <vvinod@tavisca.in>
2019-08-12 11:25:30 +0530

    Dealing with positives and zero done

--- src/main/java/SequenceFinder.java
+++ src/main/java/SequenceFinder.java
@@ -1,5 +1,6 @@
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SequenceFinder {
     public static int findSequence(int[] ints) {
@@ -20,7 +21,6 @@ public class SequenceFinder {
             {
                 for(int i=0; i<numberOfNegatives-1;i++)
                 {
-                    System.out.println("inside");
                     sum=sum+(ints[i]*ints[i+1]);
                     i++;
                 }
@@ -29,15 +29,64 @@ public class SequenceFinder {
             {
                 for(int i=0; i<numberOfNegatives-1;i++)
                 {
-                    System.out.println("inside");
                     sum=sum+(ints[i]*ints[i+1]);
                     i++;
                 }
                 sum=sum+ints[numberOfNegatives-1];
             }
         }
+        int numberOfPositives=findPostivies(ints);
+        int postives[]=makePosArray(ints,numberOfPositives);
+        if(numberOfPositives%2==0&&numberOfPositives!=0)
+        {
+            if(postives[0]*postives[1]>postives[0]+postives[1])
+            {
+                sum=sum+postives[0]*postives[1];
+            }
+            else
+            {
+                sum=sum+postives[0]+postives[1];
+            }
+            for(int i=2;i<postives.length;i++)
+            {
+                sum=sum+postives[i]*postives[i+1];
+                i++;
+            }
+        }
+        else if(numberOfPositives!=0 && numberOfPositives%2!=0)
+        {
+            sum=sum+postives[0];
+            for(int i=1;i<postives.length;i++)
+            {
+                sum=sum+(postives[i]*postives[i+1]);
+                i++;
+            }
+        }
         System.out.print(sum);
         return sum;
+    }
+    public static int[] makePosArray(int[] ints, int number)
+    {
+        int [] pos=new int[number];
+        int j=0;
+        for(int i=ints.length-number;i<ints.length;i++)
+        {
+            pos[j]=ints[i];
+            j++;
+        }
+        return pos;
+    }
+    public static int findPostivies(int[] ints)
+    {
+        int count=0;
+        for(int i:ints)
+        {
+            if(i>0)
+            {
+                count++;
+            }
+        }
+        return count;
     }
     public static int findNegatives(int[] ints)
     {
